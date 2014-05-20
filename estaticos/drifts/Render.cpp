9ca6e370f9fedb99fe09e333728c88077ca7a2c6
RodrigoNC <rodrigolnr@gmail.com>
2014-05-20 18:15:13 -0300

    fixed conflict master e fixed includes.h

--- estaticos/drifts/Render.cpp
+++ estaticos/drifts/Render.cpp
@@ -1,12 +1,13 @@
 void Render(){
 	SDL_RenderClear(Renderer);
-<<<<<<< HEAD
+  
   //SDL_Surface *image = IMG_Load ("/home/clara/Documents/UFRN/2014.1/EDBI/drifts/estaticos/images/test.jpg"); //Clara
   SDL_Surface *image = IMG_Load("C:/Users/homepc/Downloads/rod/prog/drifts/estaticos/images/test.jpg"); //Rod
 	if( !image ){
     std::cout<<"IMG_Load: "<<IMG_GetError();
     return;
   }
+
   SDL_Rect rcDest = { 0, 0, 0, 0 };
   SDL_BlitSurface ( image, NULL, PrimarySurface, &rcDest );
   SDL_Texture * testura = SDL_CreateTextureFromSurface(Renderer, image);
@@ -14,22 +15,7 @@ void Render(){
   
   
   SDL_RenderCopy(Renderer, testura, NULL, NULL);
-=======
-	//SDL_Surface *image = IMG_Load ("/home/clara/Documents/UFRN/2014.1/EDBI/drifts/estaticos/images/test.jpg");
-	//SDL_Surface *image = IMG_Load("C:/Users/homepc/Downloads/rod/prog/drifts/estaticos/images/test.jpg");
-  if ( !image ){
-      std::cout<<"IMG_Load: "<<IMG_GetError();
-      return;
-    }
-    SDL_Rect rcDest = { 0, 0, 0, 0 };
-    SDL_BlitSurface ( image, NULL, PrimarySurface, &rcDest );
-    SDL_Texture * testura = SDL_CreateTextureFromSurface(Renderer, image);
-    SDL_FreeSurface ( image );
-    
 
-
-    SDL_RenderCopy(Renderer, testura, NULL, NULL);
->>>>>>> ab38e433afc2bcac755e1fce79963e8129bded01
     
 	SDL_RenderPresent(Renderer);
 }
--- estaticos/includes.h
+++ estaticos/includes.h
@@ -2,29 +2,19 @@
 #include <SDL2/SDL_image.h>
 
 #include <iostream>
-<<<<<<< HEAD
 #include <jpeg-9a/jpeglib.h> //Windows
 //#include <jpeglib.h> //Unity
-=======
-#include <jpeglib.h>
-//#include <jpeg-9a/jpeglib.h>
-//#include <libpng12/libpng>
->>>>>>> ab38e433afc2bcac755e1fce79963e8129bded01
+
 
 #include "Log.h"
+
 
 #include "texture/Texture.h"
 #include "texture/Texture.cpp"
 
 
-#include "drifts/Execute.cpp"
 #include "drifts/Init.cpp"
 #include "drifts/Loop.cpp"
 #include "drifts/OnEvent.cpp"
 #include "drifts/Render.cpp"
 #include "drifts/Cleanup.cpp"
-
-#include "drifts/GetRenderer.cpp"
-#include "drifts/GetInstance.cpp"
-#include "drifts/GetWindowWidth.cpp"
-#include "drifts/GetWindowHeight.cpp"
