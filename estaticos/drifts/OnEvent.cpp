d47741b4a320c3235a5b38bee6fcfd0979cef37a
RodrigoNC <rodrigolnr@gmail.com>
2014-05-27 16:01:02 -0300

    on mouse button credits to menu.

--- estaticos/drifts/OnEvent.cpp
+++ estaticos/drifts/OnEvent.cpp
@@ -2,7 +2,19 @@ void OnEvent(SDL_Event* Event){
 	if(Event->type == SDL_QUIT) {
         Running = false;
     }
-    if(Event->type == SDL_MOUSEMOTION){
-        SDL_GetMouseState(&playerRect.x, &playerRect.y);
+    if(screen == 0){
+    	if(Event->type == SDL_MOUSEBUTTONDOWN && Event->button.button == SDL_BUTTON_LEFT){
+	        screen = 1;
+    	}
     }
+    else if(screen == 1){
+
+    }
+    else if(screen == 2){
+    	if(Event->type == SDL_MOUSEMOTION){
+        	SDL_GetMouseState(&playerRect.x, &playerRect.y);
+    	}
+    }
+    
+    
 }
