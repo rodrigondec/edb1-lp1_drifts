21a1d314f0c2063547c2a189a5092e3413388488
RodrigoNC <rodrigolnr@gmail.com>
2014-06-03 05:57:15 -0300

    per tick. 1 bola ou rand() bolas??

--- estaticos/drifts/Loop.cpp
+++ estaticos/drifts/Loop.cpp
@@ -1,7 +1,13 @@
 void Drifts::Loop(){
 	if(screen == 2){
 		if(get_secs()%clock_spawn == 0 && checker_create_bola){
-        Create_Bola();
+			int i = (rand()%3)+1;
+			while(i > 0){ //random 1~3 bolas por tick
+				Create_Bola();
+				i -= 1;
+			}
+			
+			//Create_Bola(); //1 bola por tick
         checker_create_bola = 0;
 	    }
 	    else if(get_secs()%clock_spawn != 0){
